387bbfbe42618e2ea7b9f6c72fe71982e1733665
retheesh-git2 <142608938+retheesh-git2@users.noreply.github.com>
2023-08-25 15:58:14 +0000

    Updated provider version, added code for Win VM

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -113,7 +113,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  size                  = "Standard_DS1_v2"
+  size                  = "Standard_B1s"
  
   os_disk {
     name                 = "myOsDisk"
@@ -139,5 +139,41 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
  
   boot_diagnostics {
     storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "windows_vm" {
+  name                = "MyWindowsVM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic.id
+  ]
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
+
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "MyWindowsVM-nic"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
